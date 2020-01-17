d43f2e280ee17039bd5cec53dd675c8b58a8019f
Zachary Weisburg <WeisbuZ2431@absstudents.abs.misd.net>
2020-01-17 12:30:46 -0500

    Update example1.py
    Gio Help!

--- example1.py
+++ example1.py
@@ -6,20 +6,15 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'E1'
-strategy_name = 'Betray'
-strategy_description = 'Always betray.'
+team_name = 'Zach^2'
+strategy_name = 'RandomBetray'
+strategy_description = '2/3 chance to betray, 1/3 chance to collude'
+
+import random
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    
-    #This example player always betrays.      
-    return 'b'
+    x = return random.choice(1, 3)
+    if x = 1 or x = 2:
+        return "b"
+    else:
+        return "c"
